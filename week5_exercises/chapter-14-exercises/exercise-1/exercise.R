27e749238aee9320f3d73090a9424df62a0183af
Larry-Tian <ganlin@uw.edu>
2020-02-06 16:38:00 -0800

    uploaded week 5 exercises

---
+++ week5_exercises/chapter-14-exercises/exercise-1/exercise.R
@@ -0,0 +1,74 @@
+# Exercise 1: reading and querying a web API
+
+# Load the httr and jsonlite libraries for accessing data
+# You can also load `dplyr` if you wish to use it
+library("jsonlite")
+library(dplyr)
+
+# Create a variable base_uri that stores the base URI (as a string) for the 
+# Github API (https://api.github.com)
+
+base_uri <- "https://api.github.com"
+
+# Under the "Repositories" category of the API documentation, find the endpoint 
+# that will list _repos in an organization_. Then create a variable named
+# `org_resource` that stores the endpoint for the `programming-for-data-science`
+# organization repos (this is the _path_ to the resource of interest).
+
+username <- "programming-for-data-science"
+org_resource <- paste0("/orgs/", username, "/repos")
+
+# Send a GET request to this endpoint (the `base_uri` followed by the 
+# `org_resource` path). Print the response to show that your request worked. 
+# (The listed URI will also allow you to inspect the JSON in the browser easily).
+
+uri <- paste0(base_uri, org_resource)
+response <- GET(uri)
+print(response)
+
+# Extract the content of the response using the `content()` function, saving it
+# in a variable.
+
+content_response <- content(response, "text")
+print(content_response)
+
+# Convert the content variable from a JSON string into a data frame.
+
+json_frame <- fromJSON(content_response)
+View(json_frame)
+
+# How many (public) repositories does the organization have?
+## 20?? I am not so sure how to do this
+
+# Now a second query:
+# Create a variable `search_endpoint` that stores the endpoint used to search 
+# for repositories. (Hint: look for a "Search" endpoint in the documentation).
+
+search_endpoint <- "/search/repositories"
+
+# Search queries require a query parameter (for what to search for). Create a 
+# `query_params` list variable that specifies an appropriate key and value for 
+# the search term (you can search for anything you want!)
+
+query_params <- "?q=tetris+language:assembly&sort=stars&order=desc"
+
+# Send a GET request to the `search_endpoint`--including your params list as the
+# `query`. Print the response to show that your request worked.
+
+query_uri <- paste0(base_uri, query_params)
+GET(query_uri)
+
+# Extract the content of the response and convert it from a JSON string into a
+# data frame. 
+
+query_json <- fromJSON((query_uri))
+View(query_json)
+
+# How many search repos did your search find? (Hint: check the list names to 
+# find an appropriate value).
+
+## See a list of 32 instead of a data frame.....not sure what to search for
+
+# What are the full names of the top 5 repos in the search results?
+
+## current user url, authorizations url, code_search url, cuurent_user_authority_url, commit_search_url
